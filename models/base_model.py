a5e30d63bd9b2206e2c4eea50d871cc4648af6c9
angel19951 <2384@holbertonschool.com>
2021-02-11 18:45:41 +0000

    Fixing base class

--- models/base_model.py
+++ models/base_model.py
@@ -29,8 +29,8 @@ class BaseModel():
         if len(kwargs) == 0:
 
            self.id = str(uuid.uuid4())
-           self.created_at = datetime.now().isoformat()
-           self.updated_at = datetime.now().isoformat()
+           self.created_at = datetime.now()
+           self.updated_at = datetime.now()
         else:
             for k, v in kwargs.items():
                 self.__dict__[k] = v
@@ -58,4 +58,6 @@ class BaseModel():
         """
         dict = self.__dict__.copy()
         dict["__class__"] = self.__class__.__name__
+        dict["created_at"] = self.created_at.isoformat()
+        dict["updated_at"] = self.updated_at.isoformat()
         return dict
