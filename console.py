447cfddd6b1e44b0b11d056e11fa1442883ce529
angel19951 <2384@holbertonschool.com>
2021-02-13 00:52:19 +0000

    Adding helper methods

--- console.py
+++ console.py
@@ -25,6 +25,8 @@ class HBNBCommand(cmd.Cmd):
             print("Documented commands (type help <topic>):")
             print("=========================================")
             print("EOF " + " help " + " quit")
+            print("create " + " update " + " destroy ")
+            print("update " + "all")
             print()
         try:
             topic = getattr(self, "help_" + arg)
@@ -49,26 +51,42 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class doesn't exist **")
 
+    def help_create(self):
+        """
+        Prints help screen for create command
+        """
+        print("Create command to create new class instance")
+        print("Usage: create <class_name>")
+        print()
+
     def do_show(self, arg):
-       """
-       print string representation of instance based on class and id
-       """
-       arg_list = []
-       if not arg:
-           print("** class name missing **")
-           return
-       arg_list = arg.split()
-       if arg_list[0] in storage.my_classes.keys():
-           if len(arg_list) < 2:
-               print ("** instance id missing **")
-               return
-           input_key = arg_list[0] + "." + arg_list[1]
-           if input_key in storage.all():
-               print(storage.all()[input_key])
-           else:
-               print("** no instance found **")
-       else:
-           print("** class doesn't exist **")
+        """
+        print string representation of instance based on class and id
+        """
+        arg_list = []
+        if not arg:
+            print("** class name missing **")
+            return
+        arg_list = arg.split()
+        if arg_list[0] in storage.my_classes.keys():
+            if len(arg_list) < 2:
+                print("** instance id missing **")
+                return
+            input_key = arg_list[0] + "." + arg_list[1]
+            if input_key in storage.all():
+                print(storage.all()[input_key])
+            else:
+                print("** no instance found **")
+        else:
+            print("** class doesn't exist **")
+
+    def help_show(self):
+        """
+        Prints help screen for show command
+        """
+        print("Show command to show string rep of an instance")
+        print("Usage: show <class_name>")
+        print()
 
     def do_all(self, arg):
         """
@@ -84,53 +102,79 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class doesn't exist **")
 
+    def help_all(self):
+        """
+        Prints help screen for show command
+        """
+        print("All command to print all objects of a certain type or all"
+              " objects")
+        print("Usage: all <object>")
+        print()
+
     def do_destroy(self, arg):
-       arg_list = []
-       if not arg:
-           print("** class name missing **")
-           return
-       arg_list = arg.split()
-       if arg_list[0] in storage.my_classes.keys():
-           if len(arg_list) < 2:
-               print ("** instance id missing **")
-               return
-           input_key = arg_list[0] + "." + arg_list[1]
-           if input_key in storage.all():
-               del storage.all()[input_key]
-               storage.save()
-           else:
-               print("** no instance found **")
-       else:
-           print("** class doesn't exist **")
+        arg_list = []
+        if not arg:
+            print("** class name missing **")
+            return
+        arg_list = arg.split()
+        if arg_list[0] in storage.my_classes.keys():
+            if len(arg_list) < 2:
+                print("** instance id missing **")
+                return
+            input_key = arg_list[0] + "." + arg_list[1]
+            if input_key in storage.all():
+                del storage.all()[input_key]
+                storage.save()
+            else:
+                print("** no instance found **")
+        else:
+            print("** class doesn't exist **")
+
+    def help_destroy(self):
+        """
+        Prints help screen for destroy command
+        """
+        print("Destroy command to destroy/delete an instance of "
+              "a class and id")
+        print("Usage: destroy <class_name>")
+        print()
 
     def do_update(self, arg):
-       """Updates an instance based on the class name and id by adding
-       or updating attribute
-       """
-       if not arg:
-           print("** class name missing **")
-           return
-       arg_list = arg.split()
-       if arg_list[0] in storage.my_classes.keys():
-           if len(arg_list) < 2:
-               print ("** instance id missing **")
-               return
-           input_key = arg_list[0] + "." + arg_list[1]
-           if input_key not in storage.all():
-               print("** no instance found **")
-               return
-           if len(arg_list) < 3:
-               print("** attribute name missing **")
-               return
-           if len(arg_list) < 4:
-               print("** value missing **")
-               return
-           if arg_list[2] in storage.all()[input_key].__dict__:
-               arg_list[3] = type(storage.all()[input_key].__dict__[arg_list[2]])(arg_list[3])
-           storage.all()[input_key].__dict__[arg_list[2]] = json.loads(arg_list[3])
-           storage.save()
-       else:
-           print("** class doesn't exist **")
+        """Updates an instance based on the class name and id by adding
+        or updating attribute
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        arg_list = arg.split()
+        if arg_list[0] in storage.my_classes.keys():
+            if len(arg_list) < 2:
+                print("** instance id missing **")
+                return
+            input_key = arg_list[0] + "." + arg_list[1]
+            if input_key not in storage.all():
+                print("** no instance found **")
+                return
+            if len(arg_list) < 3:
+                print("** attribute name missing **")
+                return
+            if len(arg_list) < 4:
+                print("** value missing **")
+                return
+            if arg_list[2] in storage.all()[input_key].__dict__:
+                arg_list[3] = type(storage.all()[input_key].__dict__[arg_list[2]])(arg_list[3])
+                storage.all()[input_key].__dict__[arg_list[2]] = json.loads(arg_list[3])
+                storage.save()
+        else:
+            print("** class doesn't exist **")
+
+    def help_update(self):
+        """
+        Prints help screen for update command
+        """
+        print("Update command to update/add to instance")
+        print("Usage: update <class_name> <id>")
+        print()
 
     def emptyline(self):
         """
