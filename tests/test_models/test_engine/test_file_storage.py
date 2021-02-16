b1c6e8643fc0789afc8d0c95753efe73f002c8ac
angel19951 <2384@holbertonschool.com>
2021-02-16 17:44:39 +0000

    File Storage tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -36,10 +36,32 @@ class TestFileStorage(unittest.TestCase):
         """
         Test new method to validate it run correctly
         """
+        self.all_storage = storage.all()
+        storage.new(self.my_model)
+        self.new_dict = storage.all()
+        self.assertEqual(self.all_storage, self.new_dict)
+
     def testAll(self):
         """
         Test all method to validate it runs correctly
         """
-        self.my_model.save()
         self.my_dict = storage.all()
         self.assertEqual(type(self.my_dict),  dict)
+
+    def testSave(self):
+        """
+        Test save method to validate it works correctly
+        """
+        self.dict_save = storage.save()
+        storage.save()
+        self.new_save = storage.save()
+        self.assertEqual(self.dict_save, self.new_save)
+
+    def testReload(self):
+        """
+        Test reload method to validate it works correctly
+        """
+        self.file_reload = storage.reload()
+        storage.reload()
+        self.file_save = storage.reload()
+        self.assertEqual(self.file_reload, self.file_save)
