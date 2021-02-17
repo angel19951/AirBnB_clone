d44dd2c6dacb82a2d08a0fbe43bf9336e940c5ae
Lord-Gusarov <2367@holbertonschool.com>
2021-02-17 02:23:28 -0400

    Fixes indentation in models/my_model

--- models/my_models.py
+++ models/my_models.py
@@ -13,6 +13,6 @@ from models.review import Review
 from models.user import User
 
 
-classes = {'BaseModel': BaseModel, "Amenity": Amenity, "City": City,
-            'State': State, "Place": Place, "Review": Review,
-            "User": User}
+classes = {"BaseModel": BaseModel, "Amenity": Amenity, "City": City,
+           "State": State, "Place": Place, "Review": Review,
+           "User": User}
