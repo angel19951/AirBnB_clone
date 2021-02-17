15304abd8fd27372c121505999c6029f558b0617
GorillaFu <jared@beguelin.com>
2021-02-17 02:20:28 +0000

    make dictionary global testing for check 9

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,6 +9,9 @@ from models.state import State
 from models.place import Place
 from models.review import Review
 from models.user import User
+classes = {'BaseModel': BaseModel, "Amenity": Amenity, "City": City,
+               'State': State, "Place": Place, "Review": Review,
+               "User": User}
 
 
 class FileStorage:
@@ -17,9 +20,9 @@ class FileStorage:
     """
     __file_path = 'file.json'
     __objects = dict()
-    classes = {'BaseModel': BaseModel, "Amenity": Amenity, "City": City,
+    """classes = {'BaseModel': BaseModel, "Amenity": Amenity, "City": City,
                'State': State, "Place": Place, "Review": Review,
-               "User": User}
+               "User": User}"""
 
     def __init__(self, *args, **kwargs):
         """Initializer
